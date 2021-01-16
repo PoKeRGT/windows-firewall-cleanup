03f95c58795d517e2bf241d6c7475f38ad4fda13
Yousha Aleayoub <yousha.a@hotmail.com>
2021-01-16 22:21:08 +0330

    Remove unused ref. Cleanup code.

--- FirewallCleanup/Program.cs
+++ FirewallCleanup/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FirewallCleanup
+﻿namespace FirewallCleanup
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            FWIfz fwi = new FWIfz();
-        }
-    }
+   internal static class Program
+   {
+      private static void Main(string[] args)
+      {
+         new FWIfz();
+      }
+   }
 }
